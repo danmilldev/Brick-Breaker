4c0f4f3a81df1915a31e5806aee81d71140977f7
danmilldev <silbercrisis63@gmail.com>
2024-05-17 13:08:17 +0200

    added player paddle

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,8 +1,27 @@
 import Phaser from "phaser";
 
+let colors = {
+    white: 0xffffff,
+    black: 0x000000,
+}
+
 class Game extends Phaser.Scene
 {
     constructor() {super({ key: "GameScene"})}
+
+
+    create()
+    {
+        let {width, height} = this.sys.game.canvas
+        const paddleWidth = 150
+        const paddleHeight = 20
+
+        // player paddle
+        const paddle = this.add.rectangle((width / 2) - (paddleHeight / 2), height - paddleHeight, paddleWidth, paddleHeight, colors.white)
+        paddle.setName("PlayerPaddle")
+        this.physics.add.existing(paddle, true)
+        
+    }
 
 }
 
